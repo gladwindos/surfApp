757a487aa9de82dfc2e16619e3ddebf94f752e57
Gladwin Dosunmu <gladwindosunmu@Gladwins-MacBook-Air-6.local>
2020-05-10 19:59:58 +0100

    production changes

---
+++ client/build/precache-manifest.f351dd32046845f4a95fbecd817f9888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18273844c860bcec7f0cae16d61d56ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c7376616f1abc426fa1",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "058154132917de52f3f5",
+    "url": "/static/css/main.97ffb599.chunk.css"
+  },
+  {
+    "revision": "7c7376616f1abc426fa1",
+    "url": "/static/js/2.9cc9f446.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9cc9f446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "058154132917de52f3f5",
+    "url": "/static/js/main.afb9026f.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/spots', require('./routes/api/spots'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+        );
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
